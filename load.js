654a373852a94061cff82eeb6dc6d36f43eabd2c
KivsoftOrganization <78277084+KivsoftOrganization@users.noreply.github.com>
2021-01-31 01:34:07 +0200

    Add files via upload

---
+++ load.js
@@ -0,0 +1,147 @@
+// ==UserScript==
+// @name         KivAgario
+// @version      1
+// @description  Made by KivSoft
+// @author       Kivsoft
+// @match        *://agar.io/*
+// @grant        none
+// @run-at       document-start
+// @icon         https://play.nintendo.com/images/Masthead_Kirby.17345b1513ac044897cfc243542899dce541e8dc.9afde10b.png
+// @require      http://127.0.0.1:5500/sdfsdf.js
+// ==/UserScript==
+
+/*
+Cock and ball torture (CBT), penis torture or dick torture is a sexual activity involving application of pain or constriction to the penis or testicles. This may involve directly painful activities, such as genital piercing, wax play, genital spanking, squeezing, ball-busting, genital flogging, urethral play, tickle torture, erotic electrostimulation, kneeing or kicking.[1] The recipient of such activities may receive direct physical pleasure via masochism, or emotional pleasure through erotic humiliation, or knowledge that the play is pleasing to a sadistic dominant. Many of these practices carry significant health risks.[2]
+
+
+Contents
+1	Devices and practices
+1.1	Ball stretcher
+1.2	Ball crusher
+1.3	Parachute
+1.4	Humbler
+1.5	Testicle cuffs
+1.6	Cock harness
+1.7	Ball busting
+2	CBT in Japan
+3	Safety
+4	See also
+5	References
+Devices and practices
+
+
+
+Top left: Erotic electrostimulation. Top right: Wax play. Bottom: Chastity piercing.
+Similar to many other sexual activities, CBT can be performed using toys and devices to make the penis and testicles more easily accessible for attack, or for foreplay purposes.[3][4]
+
+Ball stretcher
+
+A metal ball stretcher and cock ring, which forces penis' erection.
+A ball stretcher is a sex toy that is used to elongate the scrotum and provide a feeling of weight pulling the testicles away from the body. This can be particularly enjoyable for the wearer as it can make an orgasm more intense, as testicles are prevented from moving up. Intended to make one's testicles permanently hang much lower than before (if used regularly for extended periods of time), this sex toy can be potentially harmful to the male genitals as the circulation of blood can be easily cut off if over-tightened.
+
+While leather stretchers are most common, other models consist of an assortment of steel rings that fastens with screws, causing additional but only mildly uncomfortable weight to the wearer's testicles. The length of the stretcher may vary from 1-4 inches.[5] A more dangerous type of ball stretcher can be home-made simply by wrapping rope or string around one's scrotum until it is eventually stretched to the desired length.
+
+Ball crusher
+A ball crusher is a device made from either metal or often clear acrylic that squeezes the testicles slowly by turning a nut or screw. How tight it is clamped depends on the pain tolerance of the person it is used on. A ball crusher is often combined with bondage, either with a partner or by oneself.
+
+Parachute
+A parachute is a small collar, usually made from leather, which fastens around the scrotum, and from which weights can be hung. It is conical in shape, with three or four short chains hanging beneath, to which weights can be attached.
+
+Used as part of cock and ball torture within a BDSM relationship, the parachute provides a constant drag, and a squeezing effect on the testicles. Moderate weights of 3–5 kg can be suspended, especially during bondage, though occasionally much heavier weights are used. Smaller weights can be used when the male wearing it is free to move; the swinging effect of the weight can restrict sudden movements, as well as providing a visual stimulus for the dominant partner.[citation needed]
+
+Humbler
+
+Man with humbler
+A humbler is a BDSM physical restraint device used to restrict the movement of a submissive male participant in a BDSM scene.
+
+The humbler consists of a testicle cuff device that clamps around the base of the scrotum, mounted in the center of a bar that passes behind the thighs at the base of the buttocks. This forces the wearer to keep his legs folded forward, as any attempt to straighten the legs even slightly pulls hard on the scrotum, causing anything from considerable discomfort to extreme pain.
+
+Testicle cuffs
+A testicle cuff is a ring-shaped device around the scrotum between the body and the testicles which when closed does not allow the testicles to slide through it. A common type has two connected cuffs, one around the scrotum and the other around the base of the penis. They are just one of many devices to restrain the male genitalia. A standard padlock, which cannot be removed without its key, may also be locked around the scrotum.
+
+Some passive men enjoy the feeling of being "owned", while dominant individuals enjoy the sense of "owning" their partners. Requiring such a man wear testicle cuffs symbolizes that his sexual organs belong to his partner, who may be either male or female. There is a level of humiliation involved, by which they find sexual arousal. The cuffs may even form part of a sexual fetish of the wearer or his partner.[citation needed]
+
+However, these are extreme uses of testicle cuffs. More conventionally, the device pulls down the testicles and keeps them there during stimulation, which has a number of benefits:
+
+Making the penis appear longer. Pulling the testicles down and away from the base of the penis stretches the skin over the base of the penis and pubic bone, exposing the additional inch or so of penile shaft that is normally hidden from view.
+Improving sexual arousal. While some men may be aroused by the feeling of being "owned", the physical feeling of stretching the ligaments that suspend the testicles has an effect similar to the more common practice of stretching one's legs and pointing the toes.
+Preventing the testicles from lifting up so far that they become lodged under the skin immediately adjacent to the base of the penis, a condition which can be very uncomfortable, especially if the testicle is then squashed by the slap of skin during thrusting in sexual intercourse.
+Delaying or intensifying ejaculation by preventing the testicles from rising normally to the "point of no return". It is much harder to reach an orgasm.
+Cock harness
+A cock harness is a penile sex toy designed to be worn around the penis and scrotum. Its function is similar to that of a cock ring. These devices are often associated with BDSM activities. The Gates of Hell is a male chastity device made up of multiple cock rings that can be used for CBT.[6] Kali's Teeth is a metal bracelet with interior spikes that closes around the penis and can be used for preventing or punishing erections.[7]
+
+Ball busting
+
+Ball busting: A woman kicking male submissive's testicles at Folsom Street Fair, USA
+"Ball busting" is the practice of kicking or kneeing men in the testicles. It carries several medical risks, including the danger of testicular rupture from blunt trauma.
+
+CBT in Japan
+Tamakeri (玉蹴り) (lit. ball kicking) is a sexual fetish and subgenre of BDSM within which a man's testicles are abused. The genre is also referred to as ballbusting ("bb" for short).[8][9] Tamakeri is the Japanese term, but it is used by many non-Japanese people to describe media where Asian people—mainly women—are participating in it. The dynamics of tamakeri consist of a masochist having their testicles hurt by a sadist.[10] The fetish is popular among heterosexual and homosexual men and women.[11]
+
+Denkianma (電気按摩) (lit. "electric massage") is a popular Japanese prank played between two people where one person puts their foot into the genital area of the other and shakes it in a vibrating motion.[12][13] Often this is done by grabbing the other person's feet, raising them, and then placing one's own foot on their crotch and vibrating it. This is often done between school aged boys as a prank similar to kancho and could be seen by a western audience as a type of bullying. In 2006, Frito Lay released a special, Taitsukun-themed edition of Doritos chips, that referenced denki anma.[14][15]
+
+Safety
+
+This section needs attention from an expert in health. The specific problem is: concerns medical topics, but lacks appropriate citations. WikiProject Health may be able to help recruit an expert. (October 2019)
+Loss of blood flow is one of the biggest risks in cock and ball torture, which can be seen with loss of color and edemas.[16] Bondage in which the testicles are tied to something else is especially dangerous, increasing the risk of the testicles getting damaged through excessive tension or pulling.
+
+The most serious injuries are testicular rupture, testicular torsion and testicular avulsion, which are medical emergencies that require urgent medical attention.[17]
+
+
+Urethral fingering is an example of CBT which needs special precautions, such as not having a sharp nail on the finger inserted inside penis. Image shows a woman doing urethral fingering.
+
+
+
+A woman holding a bound man's penis applies electricity to his testicles at Folsom Street Fair.
+
+
+
+A gagged man's penis and scrotum are pulled using a rope at Folsom Street Fair.
+
+
+
+A dominatrix bites a gagged man's penis, while pulling and squeezing his testicles with her hand, at Folsom Street Fair.
+
+
+
+A woman wearing heels tramples the penis of a male.
+
+See also
+	Wikimedia Commons has media related to Male genital torture.
+Breast torture
+Chastity piercing
+Chastity cage
+Circumcision
+Groin attack
+Penile injury
+Pussy torture
+Urethral sounding
+References
+ Langdridge, Darren; Barker, Meg (2008). Safe, sane, and consensual: contemporary perspectives on sadomasochism. Palgrave Macmillan. p. 174. ISBN 978-0-230-51774-5. Archived from the original on 2013-06-20. Retrieved 2016-10-21.
+ Sydney Lee (2017-01-29), Ballbusting 101, retrieved 2019-05-02
+ Thomas, Sophie Saint. "Why Some Men Love Getting Their Penises Tortured". www.refinery29.com. Archived from the original on 2019-05-02. Retrieved 2019-05-02.
+ Krishnan, Manisha (2017-03-14). "We Asked a Cock and Ball Torture Expert About the Art of Dilating Urethras". Vice. Archived from the original on 2019-05-02. Retrieved 2019-05-02.
+ Hartley, Nina; Levine, I. S. (2006). Nina Hartley's Guide to Total Sex. Avery. p. 136. ISBN 978-1-58333-263-4. Archived from the original on 2013-06-20. Retrieved 2016-10-21.
+ "The Strict Leather Gates of Hell Review". Gritty Woman. 17 June 2015. Archived from the original on 22 March 2019. Retrieved 24 July 2018.
+ "Kali's Teeth Anti Erection Device". Femina Rising. 1995. Archived from the original on 2018-07-24. Retrieved 2018-07-24.
+ "tamakeri - Wiktionary". en.wiktionary.org. Archived from the original on 2019-04-02. Retrieved 2019-05-02.
+ "Word of the Week: Tamakeri". Posting Maven. March 30, 2015. Archived from the original on November 15, 2019. Retrieved 2018-12-12.
+ "What is Tamakeri? - Definition from Kinkly". Kinkly.com. Archived from the original on 2019-04-02. Retrieved 2019-05-02.
+ "Tamakeri". Japan for the Uninvited. 2006-06-23. Archived from the original on 2019-04-02. Retrieved 2019-05-02.
+ ""Denki Anma": The Japanese traditional torment that you'll be glad stays in Japan". SoraNews24. 2014-03-27. Archived from the original on 2019-05-02. Retrieved 2019-05-02.
+ "Wiki - denki anma - Danbooru". danbooru.donmai.us. Archived from the original on 2019-05-02. Retrieved 2019-05-02.
+ "A brief history of Taitsukun". taitsukun.com. 2019-02-28. Retrieved 2020-11-17.
+ "Denki Anma Attack | JapanNewbie". web.archive.org. 2013-10-09. Archived from the original on 2019-11-09. Retrieved 2019-05-02.
+ Dirk; Stein, David. "Cock Torture: Health and Safety". Deviants' Dictionary. Archived from the original on 29 December 2013. Retrieved 16 April 2011.
+ Dirk; Stein, David; Sommers, Richard. "Ball Torture: Health and Safety". Deviants' Dictionary. Archived from the original on 30 July 2013. Retrieved 16 April 2011.
+Hardy Haberman, Fetish Diva Midori. The Family Jewels: A Guide to Male Genital Play and Torment. Greenery Press, 2001. ISBN 1-890159-34-4.
+Listen to this article (9 minutes)
+MENU0:00
+Spoken Wikipedia icon
+This audio file was created from a revision of this article dated 16 October 2019, and does not reflect subsequent edits.
+(Audio help · More spoken articles)
+vte
+Outline of human sexuality
+vte
+Outline of BDSM
+*/
